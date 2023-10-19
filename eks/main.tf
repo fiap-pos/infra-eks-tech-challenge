1093a0de9d22a1d19b69c11b21865a4e1b15420e
diego <oliveiraj.diego@gmail.com>
2023-10-18 22:20:24 -0300

    feat: changes

--- eks/main.tf
+++ eks/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "name" {
-  name = "eks-cluster-name"
-  type = string
-  value = "name-of-cluster-eks"  
+  name  = "eks-cluster-name"
+  type  = "String"
+  value = "name-of-cluster-eks"
 }
--- eks/variables.tf
+++ eks/variables.tf
@@ -1,8 +1,9 @@
 variable "aws_account_id" {
   type = string
+  default = "551230249265"
 }
 
 variable "aws_region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
