f2e87a855958fa32dd816aeee1e3d4790f0845e0
diego <oliveiraj.diego@gmail.com>
2023-11-04 18:31:25 -0300

    feat: add s3 terraform backend and infra destroy pipeline

--- eks/main.tf
+++ eks/main.tf
@@ -3,3 +3,11 @@ resource "aws_ssm_parameter" "name" {
   type  = "String"
   value = "name-of-cluster-eks"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "tech-challenge-61"
+    key    = "infra-eks-tech-challenge"
+    region = "us-east-1"
+  }
+}
