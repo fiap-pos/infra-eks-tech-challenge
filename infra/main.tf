26a399fc6df204f1617ec6656975c55f4857c9ae
diego <oliveiraj.diego@gmail.com>
2023-11-08 13:03:32 -0300

    feat: add eks cluster

--- infra/main.tf
+++ infra/main.tf
@@ -1,24 +1,221 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "tech-challenge-vpc"
-  cidr = "10.0.0.0/17"
-
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+## VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/17"
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "tech-challenge-vpc"
   }
 }
 
-# resource "aws_ssm_parameter" "name" {
-#   name  = "eks-cluster-name"
-#   type  = "String"
-#   value = "name-of-cluster-eks"
-# }
+resource "aws_subnet" "private_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "tech-challenge-pv-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_security_group" "eks_cluster_sg" {
+  name_prefix = "eks-cluster-sg-"
+  vpc_id      = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "tech-challenge-sg"
+  }
+}
+
+resource "aws_security_group_rule" "eks_cluster_ingress" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_cluster_sg.id
+}
+
+resource "aws_eip" "nat_eip" {}
+
+resource "aws_nat_gateway" "eks_nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.private_subnet[0].id
+}
+
+resource "aws_route_table" "private_subnet_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "10.1.0.0/17"
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.eks_nat_gateway.id
+  }
+}
+
+resource "aws_route" "private_subnet_route" {
+  # count                  = 2
+  route_table_id         = aws_route_table.private_subnet_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.eks_nat_gateway.id
+}
+
+##EKS
+resource "aws_eks_cluster" "eks" {
+  name     = "tech-challenge-cluster"
+  role_arn = "arn:aws:iam::244071861643:role/eks_cluster_role" # Replace with your EKS service role ARN
+
+  vpc_config {
+    subnet_ids = aws_subnet.private_subnet[*].id
+  }
+}
+
+
+##ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for the ALB"
+  vpc_id      = aws_vpc.eks_vpc.id
+}
+
+resource "aws_security_group_rule" "alb_ingress" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+}
+
+resource "aws_security_group_rule" "alb_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+}
+
+resource "aws_lb" "eks_alb" {
+  name               = "eks-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.private_subnet[*].id
+  security_groups    = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_target_group" "eks_target_group" {
+  name     = "eks-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.eks_vpc.id
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "eks_listener" {
+  load_balancer_arn = aws_lb.eks_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "200"
+    }
+  }
+}
+
+
+##ATTACH ALB -> EKS
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "eks_nodes"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.private_subnet[*].id
+  instance_types  = ["t2.micro"]
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_iam_policy_attachment.eks_node_policy_attachment,
+    aws_iam_policy_attachment.eks_cni_policy_attachment,
+  ]
+}
+
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks_cluster_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "eks_cluster_policy_attachment" {
+  name       = "eks_cluster_policy_attachment"
+  roles      = [aws_iam_role.eks_cluster_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "eks_node_policy_attachment" {
+  name       = "eks_node_policy_attachment"
+  roles      = [aws_iam_role.eks_node_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_policy_attachment" "eks_cni_policy_attachment" {
+  name       = "eks_cni_policy_attachment"
+  roles      = [aws_iam_role.eks_node_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_security_group_rule" "eks_cluster_alb_ingress" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster_sg.id
+  source_security_group_id = aws_security_group.alb_sg.id
+}
 
 terraform {
   backend "s3" {
