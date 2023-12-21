c7aacb542820bcad2af795acaab6a3b1c432c4a3
Vanderson Nunes <vann.nunes@gmail.com>
2023-12-20 21:53:13 -0300

    Terraform Setup

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
-data "aws_availability_zones" "available" {}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
 
 # VPC
@@ -14,10 +11,11 @@ module "vpc" {
   version = "5.4.0"
 
   name                 = "vpc-${var.vpc_name}"
-  cidr                 = "172.16.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  cidr                 = "10.0.0.0/16"
+  azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -39,34 +37,22 @@ module "eks" {
   version = "19.21.0"
 
   cluster_name    = "${var.cluster_name}"
-  cluster_version = "1.24"
+  cluster_version = "1.28"
+  cluster_endpoint_public_access = true
+  
+  vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
-  vpc_id = module.vpc.vpc_id
-
   eks_managed_node_groups = {
-    first = {
+    one = {
+      name = "ng-${var.cluster_name}-1"
+      instance_types   =  ["${var.cluster_instance_type}"]      
+      min_capacity     = 2
+      max_capacity     = 3
       desired_capacity = 1
-      max_capacity     = 2
-      min_capacity     = 1
-      instance_type = "${var.cluster_instance_type}"
     }
   }
   
-}
-
-# Kubeconfig
-module "eks-kubeconfig" {
-  source     = "hyperbadger/eks-kubeconfig/aws"
-  version    = "2.0.0"
-  cluster_name = module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
-# Store kubeconfig locally
-resource "local_file" "kubeconfig" {
-  content  = module.eks-kubeconfig.kubeconfig
-  filename = "kubeconfig_${var.cluster_name}"
 }
 
 # Store cluster endpoint in SSM Parameter Store
