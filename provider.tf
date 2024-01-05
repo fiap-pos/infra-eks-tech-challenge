643afdde49fd812b0d5bf9fc136ba88902e17edd
Vanderson Nunes <vann.nunes@gmail.com>
2024-01-05 13:32:43 -0300

    EKS Cluster Setup: Configure parameters as secret envs

--- provider.tf
+++ provider.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
 terraform {
   required_providers {
     aws = {
